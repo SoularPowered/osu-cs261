c48bfc1d966804507e3bbc8062d12d8902d65c26
Shawn Hillyer <shawn.hillyer@gmail.com>
2016-01-05 15:59:52 -0800

    Fix Q3 rand() call and comments

--- a1/Q3.c
+++ a1/Q3.c
@@ -7,61 +7,64 @@
 #include <stdio.h>
 #include <stdlib.h> // to use malloc - getting warnings/errors without this.
 
+/* sort an array pointed to by number with n elements using bubble sort
+ * Preconditions: n >= 0, number != NULL
+ * Postconditoins: array pointed to by number will be sorted in ascending order
+ */
 void sort(int* number, int n){
     /*Sort the given array number , of length n*/
     int i;  // Loop counter
+    
+    /* Bubble Sort implementation */
+    enum boolean { false, true };
+    enum boolean swapped;
+    
+    do {
+    	swapped = false;
 
-	/* Bubble Sort implementation */
-	enum boolean { false, true };
-
-	enum boolean swapped;
-
-	do {
-		swapped = false;
-
-		/* Check pairs and swap if any number at index i is greater than the value at i+1 */
-		for (i = 0; i < n - 1; i++) {
-
-			if (number[i] > number[i+1]) {
-				/* Swap the values */
-				int temp = number[i];
-				number[i] = number[i + 1];
-				number[i + 1] = temp;
-
-				swapped = true;
-			}
+	/* Check pairs and swap if any number at index i is greater than the value at i+1 */
+	for (i = 0; i < n - 1; i++) {
+		if (number[i] > number[i+1]) {
+			/* Swap the values */
+			int temp = number[i];
+			number[i] = number[i + 1];
+			number[i + 1] = temp;
+	
+			swapped = true;
 		}
-	} while (swapped);
+	}
+    } while (swapped);
 }
 
 int main(){
     /*Declare an integer n and assign it a value of 20.*/
-	int n = 20;
-	int i; // counter for loops
+    int n = 20;
+    int i; // counter for loops
 
     /*Allocate memory for an array of n integers using malloc.*/
-	int* values = (int *)malloc(n * sizeof(int));
+    int* values = (int *)malloc(n * sizeof(int));
 
     /*Fill this array with random numbers, using rand().*/
-	srand((unsigned)time(NULL));
-
-	for (i = 0; i < n; i++) {
-		values[i] = rand();
-	}
+    // srand((unsigned)time(NULL));
+    
+    for (i = 0; i < n; i++) {
+	values[i] = rand();
+    }
 
     /*Print the contents of the array.*/
-	for (i = 0; i < n; i++) {
-		printf("%d\n", values[i]);
-	}
+    for (i = 0; i < n; i++) {
+    	printf("%d\n", values[i]);
+    }
 
     /*Pass this array along with n to the sort() function of part a.*/
-	sort(values, n);
+    sort(values, n);
 
     /*Print the contents of the array.*/
-	printf("\nValues after sorting:\n");
-
-	for (i = 0; i < n; i++) {
-		printf("%d\n", values[i]);
-	}
+    printf("\nValues after sorting:\n");
+   
+    for (i = 0; i < n; i++) {
+    	printf("%d\n", values[i]);
+    }
+    
     return 0;
 }
