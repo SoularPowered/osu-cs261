f6869ac7c9096635abc835fd61ef00177b6b839a
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-01-10 21:05:10 -0800

    Code finished for stackapp

--- a2/stackapp.c
+++ a2/stackapp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dynArray.h"
-
+#include "assert.h" // added for assertions
 
 /* ***************************************************************
 Using stack to check for unbalanced parentheses.
@@ -31,8 +31,41 @@ char nextChar(char* s)
 */
 int isBalanced(char* s)
 {
-	/* FIXME: You will write this function */		
-	return 0;
+	/* FIXME: You will write this function */
+	assert(s != NULL);
+	/* Set up a stack for the parens */
+	struct DynArr* paren_stack = newDynArr(10);
+
+	int is_still_balanced = 1;  // bool. true until we find a mismatch
+	char next_char = nextChar(s);
+	char match_char;
+
+	while(next_char != '\0' && is_still_balanced) {
+		/* push opening parens/brackets */
+		if (next_char == '(' ||
+			next_char == '{' ||
+			next_char == '[') {
+			// push )
+			pushDynArr(paren_stack, next_char);
+		}
+
+		/* Check closing parens */
+		else if (next_char == ')' ||
+				 next_char == '}' ||
+				 next_char == ']') {
+			/* pop and see next_char matches top  */
+			match_char = topDynArr(paren_stack);
+			if (next_char != match_char) {
+				is_still_balanced = 0; // not balanced
+			}
+			/* if it does, remove the last element from array for next round */
+			else {
+				popDynArr(paren_stack);
+			}
+		}
+	}
+
+	return is_still_balanced;
 }
 
 int main(int argc, char* argv[]){
