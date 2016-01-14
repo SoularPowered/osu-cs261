9c6af74b5b4f3d405d46bc51ad4dcb538865b953
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-01-13 21:37:58 -0800

    Start ws17

---
+++ week2/src/week2.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : week2.c
+ Author      : Shawn Hillyer
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Test"); /* prints Test */
+	return EXIT_SUCCESS;
+}
