60ae894cbc4a10fac137d4fa33a51d54dd610d6d
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-02-15 23:17:20 -0800

    Finish main.c menu

--- a5/src/main.c
+++ a5/src/main.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <string.h>
 #include "toDoList.h"
-
+#define FILE_NAME_SIZE 128
 
 int main (int argc, const char * argv[])
 {
@@ -32,18 +32,24 @@ int main (int argc, const char * argv[])
       switch (cmd) {
         case 'l': // "'l' to load to-do list from a file
         {
-          // Create File object and open for reading
+          // Create File object and open for reading based on filename entered by user
           FILE *inputFile;
-          char *fileName = "todo.txt";
-          // TODO: Get fileName from user instead of static
+          char fileName[FILE_NAME_SIZE];
+          printf("Please enter the filename: ");
+          scanf("%s", fileName);
 
           inputFile = fopen(fileName, "r");
           if (inputFile == NULL) {
-            fprintf(stderr, "Cannot open %s", fileName);
+            // If error, print error message
+            fprintf(stderr, "Cannot open %s\n\n", fileName);
           }
           else {
+            // Else load the list into the heap and close file
+            printf("The list has been loaded from file successfully.\n\n");
             loadList(mainList, inputFile);
+            fclose(inputFile);
           }
+          while (getchar() != '\n'); // Clear trailing newline
           break; 
         }
 
@@ -51,31 +57,60 @@ int main (int argc, const char * argv[])
         {
           // Create file object and open for writing
           FILE *outputFile;
-          char *fileName = "newtodo.txt";
-          // TODO: Get fileName from user
+          char fileName[FILE_NAME_SIZE];
+          printf("Please enter the filename: ");
+          scanf("%s", fileName);
 
           outputFile = fopen(fileName, "w");
           if (outputFile == NULL) {
-            fprintf(stderr, "Cannot open %s", fileName);
+            // If error, print error message
+            fprintf(stderr, "Cannot open %s\n\n", fileName);
           }
           else {
+            // Else save the list to file and close file
+            printf("The list has been saved into the file successfully.\n\n");
             saveList(mainList, outputFile);
+            fclose(outputFile);
           }
+          while (getchar() != '\n'); // Clear trailing newline
           break;
         }
 
         case 'a': // "'a' to add a new task
         {
+          printf("Please enter the task description: ");
+          char description[TASK_DESC_SIZE];
+          fgets(description, TASK_DESC_SIZE, stdin);
+          // Strip the newline if one is leftover from fgets(). Most elegant solution I could find.
+          // Source of this one line of code: http://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
+          description[strcspn(description, "\n")] = 0; 
+
+          // Prompt for task priority until get a valid integer
+          int priority;
+          do {
+            printf("Please enter the task priority (0-999): ");
+            scanf("%d", &priority);
+            while (getchar() != '\n'); // Clear trailing newline
+          } while (priority < 0 || priority > 999);
+
+          TaskP newTask = createTask(priority, description);
+          addHeap(mainList, newTask, compare);
+          printf("The task \'%s\' has been added to your to-do list.\n\n", description);
           break;
         }
 
         case 'g': // "'g' to get the first task
         {
+          TaskP firstTask = getMinHeap(mainList);
+          printf("Your first task is: %s\n\n", firstTask->description);
           break;
         }
 
         case 'r': // "'r' to remove the first task
         {
+          TaskP firstTask = getMinHeap(mainList);
+          printf("Your first task \'%s\' has been removed from the list.\n\n", firstTask->description);
+          removeMinHeap(mainList, compare);
           break;
         }
 
